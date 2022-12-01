dff5586dc513e0be02b748723574340059931ddd
DrC0okie <timothee.vanhove@heig-vd.ch>
2022-12-01 11:57:53 +0100

    Group constructor works TODO: Set relative path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,9 @@ import org.json.simple.JSONObject;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Group group = new Group(5);
+
         SMTPClient client = new SMTPClient();
 
 
