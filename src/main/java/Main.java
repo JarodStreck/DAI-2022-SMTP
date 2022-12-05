f5c830dfc209f9d3aaf5113ea7dd92e49ce4eb0f
Jarod <jarod.streckeisen@heig-vd.ch>
2022-12-05 09:44:13 +0100

    Remove main error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Group group = new Group(5);
+        //Group group = new Group(5);
 
         SMTPClient client = new SMTPClient();
 
